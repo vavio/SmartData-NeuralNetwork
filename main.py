296ce2474fbfe876beda5942a93929cbeca88d70
Valentin Ambaroski <valentina@tabtale.com>
2016-04-17 16:20:12 +0200

    Updated NN
    Updated the neural network to use only 1 user

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sknn.mlp import Regressor, Layer
 
 import time
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def convert_time(hours, minutes):
@@ -17,7 +18,15 @@ def convert_time(hours, minutes):
 def preprocess_data(input_file='dataset.txt'):
     line_number = 0
     customer_map = {}
+    day_subtract = timedelta(days=1)
     week_subtract = timedelta(days=7)
+    hour_subtract = timedelta(hours=1)
+    twohour_subtract = timedelta(hours=2)
+    halfhour_subtract = timedelta(minutes=30)
+    year_subtract = timedelta(weeks=51)
+
+    plotX = []
+    plotY = []
 
     print 'PARSING DATA...'
     data = []
@@ -33,46 +42,77 @@ def preprocess_data(input_file='dataset.txt'):
         raw_data = l.split('\t')[:3]
 
         customer_id = raw_data[0]
+
+        if customer_id != '8664162':
+            continue
         isodate = datetime.strptime(raw_data[1], "%d/%m/%Y %H:%M")
         supply_usage = float(raw_data[2])
 
         if customer_id not in customer_map:
+            print customer_id
             customer_map[customer_id] = {}
 
-        # if isodate in customer_map[customer_id]:
-        #     print line_number, customer_id, isodate, customer_map[customer_id][isodate]
+        if isodate - year_subtract in customer_map[customer_id]:
+            break
+
         customer_map[customer_id][isodate] = supply_usage
 
-        previous_week = isodate - week_subtract
-        previous_week_usage = customer_map[customer_id].get(previous_week, 0)
-        # TODO: Possibly add usage in previous hours
+        previous_day_usage = customer_map[customer_id].get(isodate - day_subtract, 0)
+        previous_week_usage = customer_map[customer_id].get(isodate - week_subtract, 0)
+        previous_hour_usage = customer_map[customer_id].get(isodate - hour_subtract, 0)
+        previous_twohour_usage = customer_map[customer_id].get(isodate - twohour_subtract, 0)
+        previous_halfhour_usage = customer_map[customer_id].get(isodate - halfhour_subtract, 0)
 
-        data.append([convert_time(isodate.hour, isodate.minute), isodate.weekday(), isodate.timetuple().tm_yday,
-                     previous_week_usage, supply_usage])
+        plotX.append(isodate.timetuple().tm_yday + convert_time(isodate.hour, isodate.minute) / 1440.0)
+        plotY.append(supply_usage)
+
+        data.append([
+            convert_time(isodate.hour, isodate.minute),
+            isodate.weekday(),
+            previous_day_usage,
+            previous_week_usage,
+            previous_hour_usage,
+            previous_twohour_usage,
+            previous_halfhour_usage,
+            supply_usage])
+
+
+    print 'PLOTTING DATA...'
+    print len(plotX), len(plotY)
+    plt.figure(1)
+    max_plot = 200
+
+    plt.plot(plotX[:max_plot], plotY[:max_plot], 'bo', plotX[:max_plot], plotY[:max_plot], 'k')
+    plt.grid(True)
+    plt.title('UID 8664162')
+    plt.xlabel('Day of year')
+    plt.ylabel('Usage $[kW/h]$')
+
+    plt.show()
+    return
 
     print 'NORMALIZING DATA...'
     data = np.array(data)
     N = data.shape[0]
-    X = preprocessing.scale(data[:, :-1]).reshape(N, data.shape[1])
+    X = preprocessing.scale(data[:, :-1]).reshape(N, data.shape[1] - 1)
     Y = np.array(data[:, -1])
 
     client = MongoClient()
     db = client.smart_grid
 
-    db_cache = []
-    print 'SAVING RAW DATA TO MONGODB...'
-    for cust_id in customer_map:
-        print '\tCUSTOMER_ID {0}'.format(cust_id)
-        for cust_date in customer_map[customer_id]:
-            db_cache.append({
-                'customer_id': cust_id,
-                'datetime': cust_date,
-                'supply_usage': customer_map[customer_id][cust_date]
-            })
-
-        print len(db_cache)
-        db.user_info.insert_many(db_cache)
-        db_cache = []
+    # db_cache = []
+    # print 'SAVING RAW DATA TO MONGODB...'
+    # for cust_id in customer_map:
+    #     print '\tCUSTOMER_ID {0}'.format(cust_id)
+    #     for cust_date in customer_map[customer_id]:
+    #         db_cache.append({
+    #             'customer_id': cust_id,
+    #             'datetime': cust_date,
+    #             'supply_usage': customer_map[customer_id][cust_date]
+    #         })
+    #
+    #     db.user_info.insert_many(db_cache)
+    #     db_cache = []
 
     print 'SAVING NORMALIZED DATA TO MONGODB'
     db_cache = []
@@ -83,11 +123,15 @@ def preprocess_data(input_file='dataset.txt'):
             db_cache = []
 
         current_data = X[i, :]
+
         db_cache.append({
             'time_of_day': current_data[0],
             'week_day': current_data[1],
-            'year_day': current_data[2],
-            'previous_usage': current_data[3],
+            'previous_day_usage': current_data[2],
+            'previous_week_usage': current_data[3],
+            'previous_hour_usage': current_data[4],
+            'previous_twohour_usage': current_data[5],
+            'previous_halfhour_usage': current_data[6],
             'current_usage': Y[i]
         })
 
@@ -100,9 +144,12 @@ def get_vector(data):
         1.0,  # bias
         data['time_of_day'],
         data['week_day'],
-        data['year_day'],
-        data['previous_usage'],
-        data['current_usage']
+        data['previous_day_usage'],
+        data['previous_week_usage'],
+        data['previous_hour_usage'],
+        data['previous_twohour_usage'],
+        data['previous_halfhour_usage'],
+        data['current_usage'],
     ]
 
 
@@ -119,6 +166,13 @@ def load_data():
 
     print 'READING DATA...'
     idx = 0
+
+    count = db.user_data.find({}).count()
+    train_size = int(count * 0.70)
+    validation_size = int(count * 0.15)
+
+    print count, train_size, validation_size
+
     for data in db.user_data.find({}):
 
         if idx % 100000 == 0:
@@ -126,20 +180,21 @@ def load_data():
 
         idx += 1
 
-        rnd = np.random.rand()
         vec = np.array(get_vector(data))
         X = vec[:-1]
         y = vec[-1]
 
-        if rnd < 0.5:
+        if train_size > 0:
             trainX.append(X)
             trainY.append(y)
-        elif rnd < 0.75:
-            testX.append(X)
-            testY.append(y)
-        else:
+            train_size -= 1
+        elif validation_size > 0:
             validationX.append(X)
             validationY.append(y)
+            validation_size -= 1
+        else:
+            testX.append(X)
+            testY.append(y)
 
     print 'FINISHED READING DATA'
     return [
@@ -153,19 +208,12 @@ def train_nn(train_set, validation_set):
 
     nn = Regressor(
         layers=[
-            Layer("Tanh", units=4),
-            Layer("Rectifier", units=4),
-            Layer("Sigmoid", units=3),
-            Layer("Tanh", units=3),
-            Layer("Sigmoid", units=3),
-            Layer("Tanh", units=2),
-            Layer("Rectifier", units=2),
-            Layer("Linear", units=2),
+            Layer("Sigmoid", units=2),
             Layer("Sigmoid")
         ],
-        learning_rate=0.001,
-        batch_size=20,
-        n_iter=1000,
+        learning_rate=0.0001,
+        batch_size=5,
+        n_iter=10000,
         valid_set=validation_set,
         verbose=True,
     )
@@ -174,9 +222,18 @@ def train_nn(train_set, validation_set):
     return nn
 
 
+def mean_absolute_percentage_error(ypred, ytrue):
+    """ returns the mean absolute percentage error """
+    idx = ytrue != 0.0
+    return np.mean(np.abs(ypred[idx]-ytrue[idx])/ytrue[idx])
+
+
 start_time = time.time()
-# preprocess_data()
+preprocess_data()
 datasets = load_data()
 nn = train_nn(datasets[0], datasets[1])
-print('score =', nn.score(datasets[2][0], datasets[2][1]))
+y_pred = nn.predict(datasets[2][0])
+
+print 'mape\t= {0}'.format(mean_absolute_percentage_error(y_pred, datasets[2][1]))
+print 'score\t= {0}'.format(nn.score(datasets[2][0], datasets[2][1]))
 print time.time() - start_time
